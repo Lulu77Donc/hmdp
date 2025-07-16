11e65460dedb61885dd3ad5aaf62a10ab03825a0
lijiaxiang_hela <1259465943@qq.com>
2025-07-16 16:02:41 +0800

    java调用lua脚本改造分布式锁

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,17 +1,29 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
-public class SimpleRedisLock implements ILock{
+public class SimpleRedisLock implements ILock {
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
 
     private static final String KEY_PREFIX = "lock:";
-    private static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";//这里的true指的是将uuid中-去掉
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";//这里的true指的是将uuid中-去掉
+
+    //先加载lua脚本文件
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -21,13 +33,13 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取线程标示
-        String threadId = ID_PREFIX+Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         //获取锁
-        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX+name, threadId, timeoutSec, TimeUnit.SECONDS);
+        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);//避免空指针
     }
 
-    @Override
+    /*@Override
     public void unlock() {
         //为避免其中一个线程业务阻塞，提前释放锁，另一个线程进来获取锁，而前面的线程释放锁，可能会导致后面的线程锁提前释放掉了
         //因此在释放锁的时候需要根据uuid+线程id的value来判断是否是自己的锁
@@ -41,5 +53,14 @@ public class SimpleRedisLock implements ILock{
             stringRedisTemplate.delete(KEY_PREFIX+name);
         }
 
+    }*/
+    @Override
+    public void unlock() {
+        //调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX+name),//创建单一集合
+                ID_PREFIX+Thread.currentThread().getId());
     }
+
 }
